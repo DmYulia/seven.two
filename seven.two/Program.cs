f2e05200c0d78fae2dac0d4bc6c3eb670e2a23e8
yulad <yulad@MATEBOOK>
2022-02-10 17:28:19 +0300

    Добавьте файлы проекта.

---
+++ seven.two/Program.cs
@@ -0,0 +1,52 @@
+﻿using static System.Console;
+
+namespace _seven.two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double VCube(double a)
+            {
+                try
+                {
+                    if (a > 0) return (a * a * a);
+                    else throw new Exception("Error intput data!\nCode ");
+                }
+                catch (Exception e)
+                {
+                    Write(e.Message);
+                    return 0;
+                }
+            }
+            double SCube(double a)
+            {
+                try
+                {
+                    if (a > 0) return (a * a * 6);
+                    else throw new Exception("Error intput data!\nCode ");
+                }
+                catch (Exception e)
+                {
+                    Write(e.Message);
+                    return 0;
+                }
+            }
+            double a = 1;
+            Write("Write a: ");
+        Mm: try
+            {
+                a = Convert.ToDouble(ReadLine());
+            }
+            catch(Exception e)
+            {
+                WriteLine(e.Message);
+                WriteLine("Try again..");
+                goto Mm;
+            }
+            WriteLine(VCube(a));
+            WriteLine(SCube(a));
+            ReadKey();
+        }
+    }
+}
